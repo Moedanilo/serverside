03d6fb009fe2baa41be2a592be3ea059f09d7d05
Mo2 . <moses.uriarte1@gmail.com>
2023-12-20 03:14:25 -0500

    build server

--- index.js
+++ index.js
@@ -4,9 +4,18 @@ const app = express()
 
 const port = 3000
 
+// const clientPath = app.resolve(__dirname, '../favlinks/dist')
+
+// app.use(express.static(clientPath))
+
 app.get('/', (req, res)=>{
 res.status(200).send(`Hello World! We're live on out first server!`)
 })
+
+// app.get('/', (req, res) => {
+//     // we'll do some stuff here
+//     res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'))
+//   })
 
 app.get('/test', (req,res)=> {
     res.send(`<html><body><h1>Testing sending HTML</h1></body></html>`)
